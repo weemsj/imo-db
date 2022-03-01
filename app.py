6640c8f7b8ca6e9bcfbe13ac933772edb25162a6
Jacqueline Weems <weemsj@flip3.engr.oregonstate.edu>
2022-03-01 00:57:48 -0800

    addin update and delete fuctions to all pages

--- app.py
+++ app.py
@@ -48,23 +48,35 @@ def add_department():
         data = (dept_name, dept_total)
         db_connection = db.connect_to_database()
         execute_query(db_connection, query, data)
-        return render_template('success.html', action= 'Add department', entity='Departments', active='departments', return_page='departments') # retuns a success message
+        return render_template('success.html', action= 'Add department', entity='Departments', active='departments', return_page='/departments') # retuns a success message
 
-@app.route('/update_department/<dept_id>', methods=['POST', 'GET'])
-def update_department(dept_id):
-    db_connection = connect_to_database()
+@app.route('/update_department', methods=['POST', 'GET'])
+def update_department():
+    db_connection = db.connect_to_database()
+    dept_id = request.args.get('dept_id')
     if request.method == 'GET':
         dept_query = 'SELECT * FROM Departments WHERE dept_id = %s;' % (dept_id)
         dept_result = execute_query(db_connection, dept_query).fetchone()
-        return render_template('update_department.html', department=dept_result)
+        return render_template("update_department.html", dept_id=dept_id, department=dept_result, entity='Departments', return_page='/departments')
+
     elif request.method == 'POST':
         dept_id = request.form['dept_id']
         dept_name = request.form['dept_name']
         query = "UPDATE Departments SET dept_name = %s WHERE dept_id = %s ;"
         data = (dept_name, dept_id)
-        db_connection = db.connect_to_database()
         execute_query(db_connection, query, data)
-        return render_template('success.html', action='Update department', entity='Departments', active='departments', return_page='departments')
+        return redirect('/departments')
+
+@app.route('/delete_department')
+def delete_department():
+    dept_id = request.args.get('dept_id')
+    db_connection = db.connect_to_database()
+    query = "DELETE from Departments WHERE dept_id = %s ;"
+    data = (dept_id,)
+    execute_query(db_connection, query, data)
+    return redirect('/departments')
+
+    
 
 # -----------End Departments------------------------------------------------------------
 
@@ -72,7 +84,7 @@ def update_department(dept_id):
 @app.route('/employees')
 def employees():
     db_connection = db.connect_to_database()
-    query = "SELECT * FROM Employees;"
+    query = "SELECT emp_id, f_name, l_name, status, dept_number FROM Employees;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template('employees.html', entity=results)
@@ -110,6 +122,49 @@ def add_employee():
         data = (f_name, l_name, gender, address_1, address_2, city, state, zip, tel, email, start_date, end_date, status, dept_number)
         execute_query(db_connection, query, data)
         return render_template('success.html', action = 'Add employee', entity='Employees', active='employees', return_page='employees') # returns a success message
+
+@app.route('/update_employee', methods=['POST','GET'])
+def update_employee():
+    emp_id = request.args.get('emp_id')
+    db_connection = db.connect_to_database()
+    if request.method == 'GET':
+        emp_query = 'SELECT * FROM Employees WHERE emp_id = %s;' % (emp_id)
+        emp_result = execute_query(db_connection, emp_query).fetchone()
+        dept_query = "SELECT dept_id, dept_name FROM Departments;"
+        dept_result = execute_query(db_connection, dept_query).fetchall()
+        return render_template("update_employee.html", emp_id=emp_id, employee=emp_result, dept=dept_result, entity='Employees', return_page='/employees')
+    else:
+        f_name = request.form['f_name']
+        l_name = request.form['l_name']
+        gender = request.form['gender']
+        address_1 = request.form['address_1']
+        address_2 = request.form['address_2']
+        city = request.form['city']
+        state = request.form['state']
+        zip = request.form['zip']
+        tel = request.form['tel']
+        email = request.form['email']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        if end_date is Null:
+            status = 'ACTIVE'
+        else:
+            status = 'NOT ACTIVE'
+        dept_number = request.form['dept']
+        query = "UPDATE Employees SET f_name = %s, l_name = %s, gender = %s, address_1 = %s, address_2 = %s, city = %s, state = %s, zip = %s, tel = %s, email = %s, start_date = %s , end_date = %s, status = %s, dept_number = %s WHERE emp_id = %s;"
+        data = (f_name, l_name, gender, address_1, address_2, city, state, zip, tel, email, start_date, end_date, status, dept_number)
+        execute_query(db_connection, query, data)
+        return redirect('/employees')
+
+@app.route('/delete_employee')
+def delete_employee():
+    emp_id = request.args.get('emp_id')
+    db_connection = db.connect_to_database()
+    query = "DELETE from Employees WHERE emp_id = %s ;"
+    data = (emp_id,)
+    execute_query(db_connection, query, data)
+    return redirect('/employees')
+
 # -------------End Employees----------------------------------------------------------
 
 # ------- Certifications add, update, delete and edit ------------------------
@@ -135,6 +190,34 @@ def add_certification():
         db_connection = db.connect_to_database()
         execute_query(db_connection, query, data)
         return render_template('success.html', action = 'Add certification', entity='Certifications', active='certifications', return_page='certifications') # retuns a success message
+
+@app.route('/update_certification', methods=['POST', 'GET'])
+def update_certification():
+    db_connection = db.connect_to_database()
+    cert_id = request.args.get('cert_id')
+    if request.method == 'GET':
+        cert_query = 'SELECT * FROM Certifications WHERE cert_id = %s;' % (cert_id)
+        cert_result = execute_query(db_connection, cert_query).fetchone()
+        print(cert_result)
+        return render_template("update_certification.html", cert_id=cert_id, cert=cert_result, return_page='/certifications', entity='Certification')
+
+    elif request.method == 'POST':
+        cert_id = request.form['cert_id']
+        cert_name = request.form['cert_name']
+        query = "UPDATE Certifications SET cert_name = %s WHERE cert_id = %s ;"
+        data = (cert_name, cert_id)
+        execute_query(db_connection, query, data)
+        return redirect('/certifications')
+
+@app.route('/delete_certification')
+def delete_certification():
+    cert_id = request.args.get('cert_id')
+    db_connection = db.connect_to_database()
+    query = "DELETE from Certifications WHERE cert_id = %s ;"
+    data = (cert_id,)
+    execute_query(db_connection, query, data)
+    return redirect('/certifications')
+
 # -----------End Certifications------------------------------------------------------------
 
 # ------- Jobs add, update, delete and edit ------------------------
@@ -171,7 +254,30 @@ def add_job():
         # put data in a tuple and execute query
         data = (dept_number, job_description)
         execute_query(db_connection, query, data)
-        return render_template('success.html', action = 'Add job', entity='Jobs', active='jobs', return_page='jobs') # retuns a success message
+        return render_template('success.html', action = 'Add job', entity='Jobs', active='jobs', return_page='jobs')
+         # retuns a success message
+
+@app.route('/update_job', methods=['POST', 'GET'])
+def update_job():
+    db_connection = db.connect_to_database()
+    job_id = request.args.get('job_id')
+    print(job_id)
+    if request.method == 'GET':
+        job_query = 'SELECT * FROM Jobs WHERE job_id = %s;' % (job_id)
+        dept_query = 'SELECT dept_id, dept_name FROM Departments;'
+        dept_result = execute_query(db_connection, dept_query).fetchall()
+        job_result = execute_query(db_connection, job_query).fetchone()
+        print(job_result)
+        return render_template("update_job.html", job_id=job_id, dept=dept_result, job=job_result, return_page='/jobs', entity='Jobs')
+
+    elif request.method == 'POST':
+        job_id = request.form['job_id']
+        job_description = request.form['job_description']
+        query = "UPDATE Jobs SET job_descriptions = %s WHERE job_id = %s ;"
+        data = (job_description, job_id)
+        execute_query(db_connection, query, data)
+        return redirect('/certifications')
+
 # ---------------End Jobs--------------------------------------------------------
 
 # ------- Classes add, update, delete and edit ------------------------
@@ -218,7 +324,7 @@ def add_class():
 def members():
     ''' Displays all members in the database '''
     db_connection = db.connect_to_database()
-    query = "SELECT * FROM Members;"
+    query = "SELECT member_id, f_name, l_name, status FROM Members;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template('members.html', entity=results)
@@ -250,16 +356,61 @@ def add_member():
     db_connection = db.connect_to_database()
     execute_query(db_connection, query, data)
     return render_template('success.html', action ='Add member', entity='Members', active='members', return_page='members')
+
+@app.route('/update_member', methods=['POST','GET'])
+def update_member():
+    member_id = request.args.get('member_id')
+    db_connection = db.connect_to_database()
+    if request.method == 'GET':
+        member_query = 'SELECT * FROM Members WHERE member_id = %s;' % (member_id)
+        member_result = execute_query(db_connection, member_query).fetchone()
+        return render_template("update_member.html", member_id=member_id, member=member_result, return_page='/members', entity='Members')
+    else:
+        f_name = request.form['f_name']
+        l_name = request.form['l_name']
+        gender = request.form['gender']
+        address_1 = request.form['address_1']
+        address_2 = request.form['address_2']
+        city = request.form['city']
+        state = request.form['state']
+        zip = request.form['zip']
+        tel = request.form['tel']
+        email = request.form['email']
+        status = request.form['status']
+        query = "UPDATE Members SET f_name = %s, l_name = %s, gender = %s, address_1 = %s, address_2 = %s, city = %s, state = %s, zip = %s, tel = %s, email = %s, status = %s;"
+        data = (f_name, l_name, gender, address_1, address_2, city, state, zip, tel, email, status) 
+        execute_query(db_connection, query, data)
+        return redirect('/members')
+
+@app.route('/delete_member')
+def delete_member():
+    member_id = request.args.get('member_id')
+    db_connection = db.connect_to_database()
+    query = "DELETE from Members WHERE member_id = %s ;"
+    data = (member_id,)
+    execute_query(db_connection, query, data)
+    return redirect('/members')
+
 # ----------------End Members-------------------------------------------------------
 
 # ------- Emp_Certs add, update, delete and edit ----------------------------------
 @app.route('/emp_certs')
 def emp_certs():
+    cert_id = request.args.get('cert_id')
     db_connection = db.connect_to_database()
-    query = "SELECT e.f_name, e.l_name, c.cert_name FROM Employees e LEFT JOIN Emp_Certs ec ON e.emp_id = ec.emp_id LEFT JOIN Certifications c ON ec.cert_id = c.cert_id WHERE c.cert_name is not NULL ORDER by e.emp_id;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = cursor.fetchall()
-    return render_template('emp_certs.html', entity=results)
+    if cert_id is None:
+        query = "SELECT e.f_name, e.l_name, c.cert_name FROM Employees e LEFT JOIN Emp_Certs ec ON e.emp_id = ec.emp_id LEFT JOIN Certifications c ON ec.cert_id = c.cert_id WHERE c.cert_name is not NULL ORDER by e.emp_id;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
+        return render_template('emp_certs.html', entity=results)
+    else:
+        query = "SELECT e.f_name, e.l_name, c.cert_name FROM Certifications c LEFT JOIN Emp_Certs ec ON c.cert_id = ec.cert_id LEFT JOIN Employees e ON ec.emp_id = e.emp_id WHERE c.cert_id = %s;"
+        data = (cert_id,)
+        cursor = db.execute_query(db_connection, query, data)
+        results = cursor.fetchall()
+        page = results[0]['cert_name'] # value is {'f_name': 'Eberto', 'l_name': 'Cashman', 'cert_name': 'NASM-CPT'}
+        print(page)  # trying to find the index for cert_name in the dictionary 
+        return render_template('emp_certs.html', entity=results, page=page) 
 
 @app.route('/add_emp_certs', methods=['POST','GET'])
 def add_emp_certs():
@@ -272,6 +423,7 @@ def add_emp_certs():
         cursor = db.execute_query(db_connection=db_connection, query=query)
         certs = cursor.fetchall()
         return render_template('add_emp_certs.html', employees=employees, certs=certs )
+
     if request.method == 'POST':
         employee = request.form['employee']
         cert = request.form['cert']
@@ -314,10 +466,10 @@ def add_mem_classes():
 @app.route('/emp_jobs')
 def emp_jobs():
     db_connection = db.connect_to_database()
-    query = "SELECT e.f_name, e.l_name, j.job_description FROM Employees e LEFT JOIN Emp_Jobs ej ON e.emp_id = ej.emp_id LEFT JOIN Jobs j ON ej.job_id = j.job_id WHERE j.job_description is not NULL ORDER by e.emp_id;"
+    query = 'SELECT d.dept_name, e.f_name, e.l_name, j.job_description FROM Employees e LEFT JOIN Emp_Jobs ej ON ej.emp_id = e.emp_id LEFT JOIN Jobs j ON ej.job_id = j.job_id LEFT JOIN Departments d ON d.dept_id = j.dept_number WHERE j.job_description is NOT NULL;'
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
-    return render_template('emp_jobs.html', entity=results)
+    return render_template('emp_jobs.html', entity=results, page='Jobs', return_page='/jobs')
 
 @app.route('/add_emp_jobs', methods=['POST','GET'])
 def add_emp_jobs():
@@ -347,24 +499,59 @@ def others():
 @app.route('/job_emps')
 def job_emps():
     """ this view shows a specific job and the employees associated with the job """
-
-    return render_template('job_emps.html')
-
-@app.route('/enrolled_classes')
-def enrolled_classes():
-    return render_template('enrolled_classes.html')
+    job_id = request.args.get('job_id')
+    db_connection = db.connect_to_database()
+    query = 'SELECT d.dept_name, e.f_name, e.l_name, j.job_description FROM Employees e LEFT JOIN Emp_Jobs ej ON ej.emp_id = e.emp_id LEFT JOIN Jobs j ON ej.job_id = j.job_id LEFT JOIN Departments d ON d.dept_id = j.dept_number WHERE j.job_description is NOT NULL and j.job_id= %s;' % (job_id)
+    cursor = db.execute_query(db_connection, query)
+    result = cursor.fetchall()
+    print(result)
+    return render_template('job_emps.html', entity = result, job_id = job_id, page='Jobs', return_page='/jobs')
 
 @app.route('/emp_details')
 def emp_details():
-    return render_template('emp_details.html')
+    emp_id = request.args.get('emp_id')
+    db_connection = db.connect_to_database()
+    query = 'SELECT * FROM Employees WHERE emp_id = %s;' % (emp_id)
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    print(results)
+    return render_template('emp_details.html', entity = results, emp_id=emp_id, page='All Employees', return_page='/employees')
 
-@app.route('/emp_dept/<dept_id>')
-def emp_dept(dept_id):
+@app.route('/enrolled_classes')
+def enrolled_classes():
+    member_id = request.args.get('member_id')
+    print(member_id)
+    db_connection = db.connect_to_database()
+    query = 'SELECT c.class_name, c.time, c.length FROM Members m LEFT JOIN Mem_Classes mc ON m.member_id = mc.member_id LEFT JOIN Classes c ON mc.class_id = c.class_id WHERE m.member_id = %s;' % (member_id)
+    member = 'SELECT f_name, l_name FROM Members WHERE member_id = %s;' % (member_id)
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    cursor = db.execute_query(db_connection=db_connection, query=member)
+    member = cursor.fetchall()
+    print(results)
+    print(member)
+    return render_template('enrolled_classes.html', entity = results, member = member)
+
+@app.route('/member_details')
+def member_details():
+    member_id = request.args.get('member_id')
+    db_connection = db.connect_to_database()
+    query = 'SELECT * FROM Members WHERE member_id = %s;' % (member_id)
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    print(results)
+    return render_template('emp_details.html', entity = results)
+
+@app.route('/emp_dept')
+def emp_dept():
+    dept_id = request.args.get('dept_id')
+    dept_name = request.args.get('dept_name')
+    print(dept_name)
     db_connection = db.connect_to_database()
     query = 'SELECT f_name, l_name FROM Employees WHERE dept_number = %s;' % (dept_id)
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
-    return render_template('emp_dept.html', entity = results)
+    return render_template('emp_dept.html', entity = results, dept_name=dept_name)
 
 @app.route('/class_details')
 def class_details():
