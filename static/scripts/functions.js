26a315b1e994db849d31456f83be8f1b0fdd0218
weemsj <71594052+weemsj@users.noreply.github.com>
2022-03-08 10:22:55 +0100

    adding scripts to dynamically search the table

--- static/scripts/functions.js
+++ static/scripts/functions.js
@@ -22,3 +22,24 @@ function empSearch() {
     }
   }
 }
+function memSearch() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("mem_search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("mem_table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
