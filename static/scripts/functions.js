ade742ded950a02adadcb7074002d067f5c8b32b
weemsj <71594052+weemsj@users.noreply.github.com>
2022-03-08 19:34:23 +0100

    added sources to functions.js

--- static/scripts/functions.js
+++ static/scripts/functions.js
@@ -24,6 +24,28 @@ function empSearch() {
   }
 }
 
+function memSearch() {
+  // Declare variables
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("mem_search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("mem_table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
 function isActive() {
   // Get the checkbox
   var checkBox = document.getElementById("Active");
